5a0d5816ca118b4b5f3d12f9dd986371b0cd160e
vishakha011 <khatadevishakha01@gmail.com>
2020-08-14 00:02:41 +0530

    Completed Assignment

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -8,10 +8,14 @@
 let firstName = "Arya";
 let lastName = "Stark";
 
+const username = "Vishakha";
+let bio = "Learning to code";
 /* Do the following using `+` operator
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
   - Alert the message `I'm Arya Stark of Winterfell` using the variable name firstName and lastName.
 */
+alert(`I am ${username} and my bio is ${bio}`);
+alert(`I'm ${firstName} ${lastName} of Winterfell`);
 
 /* Do the following using string template literal
   - Alert message saying `I am [username] and my bio is [bio]`. Replace square brackets with the value of variable.
@@ -27,6 +31,8 @@ const taxPercent = 10;
   `
   - Above message should be in two lines
 */
+alert(`The total amount is ${amount}
+ the tax amount is ${(amount * taxPercent)/100}`);
 
 /* Use double quote and escape character:
   - Alert a message saying `The total amount is [amount]
@@ -34,3 +40,4 @@ const taxPercent = 10;
   `
   - Above message should be in two lines
 */
+alert(`The total amount is ${amount} \n the tax amount is ${(amount * taxPercent)/100}`);
