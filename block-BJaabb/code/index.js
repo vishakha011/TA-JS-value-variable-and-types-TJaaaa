f634850d1e560eab3b0f6ddc83bdd4982e3c716e
vishakha011 <khatadevishakha01@gmail.com>
2020-08-17 23:00:07 +0530

    Assignment Completed

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -5,22 +5,64 @@
 3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
+let user = prompt("Enter your name");
+alert(`User: ${user}`);
+
+let userName = user;
+console.log(userName);
+console.log(user); 
+
+let age = 22, isLearning = true;
+user = "John";
+console.log(userName);
+console.log(user); 
 
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+let numA = prompt("Enter first Number");
+let numB = prompt("Enter first Number");
+let sum = Number(numA) + Number(numB);
+alert(sum);
+
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j++){
+    if(j % 2 === 0){
+        console.log(j);
+    }
+} 
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
+for(let i = 10; i >=0; i--){
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+}
 
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+i= "5";
+concatNumber = "";
+while(i >=0){
+    concatNumber += i;
+    i--;
+}
+console.log(concatNumber);
 
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+i = 10;
+concatEven = "";
+while(i >=0){
+    if(i % 2 === 0){
+        concatEven += i;
+    }
+    i--;
+}
+console.log(concatEven);
+
 
 // Comparisoin
 
@@ -29,10 +71,12 @@ for (let j = 0; j <= 10; ____) console.log(j);
 - `alert` true or false base on the entered value.
 - if the input value is anything like `true`, `null` or `undefined` alert saying `Enter a valid value`.
 
-
 Example:
 
 21, 21; // true
 "hello", -21; // false
 
 */
+let num1 = prompt("Enter first Number");
+let num2 = prompt("Enter first Number");
+Number(num1) === Number(num2) ? alert(true):alert(false);
